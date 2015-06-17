a367b525ca32845c03d5d7e6ae76d65e44ce8174
eray orçunus <erayorcunus@gmail.com>
2015-06-17 16:25:45 +0300

    lockscreen: Add option to pass swipe-up-to-unlock (2/2)
    * Option appears on PIN,pattern and password methods
    * User should press back button to see notifications, clock etc.
    * Instantly hides keyguard if Smart Lock has unlocked phone.
    
    Change-Id: I31256770869b20842c69edb4a7a57b2bad7b3ea7
    Signed-off-by: eray orçunus <erayorcunus@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    
    Conflicts:
    	src/com/android/settings/lockscreen/LockScreenSettings.java

--- src/com/android/settings/lockscreen/LockScreenSettings.java
+++ src/com/android/settings/lockscreen/LockScreenSettings.java
@@ -71,6 +71,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment
     private static final String KEY_LOCK_ENABLED = "lockenabled";
     private static final String KEY_VISIBLE_PATTERN = "visiblepattern";
     private static final String KEY_VISIBLE_ERROR_PATTERN = "visible_error_pattern";
+    private static final String KEY_DIRECTLY_SHOW = "directlyshow";
     private static final String KEY_VISIBLE_DOTS = "visibledots";
     private static final String KEY_VISIBLE_GESTURE = "visiblegesture";
     private static final String KEY_LOCK_AFTER_TIMEOUT = "lock_after_timeout";
@@ -99,6 +100,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment
     private SwitchPreference mBiometricWeakLiveliness;
     private SwitchPreference mVisiblePattern;
     private SwitchPreference mVisibleErrorPattern;
+    private SwitchPreference mDirectlyShow;
     private SwitchPreference mVisibleDots;
     private SwitchPreference mVisibleGesture;
     private SwitchPreference mPowerButtonInstantlyLocks;
@@ -107,9 +109,8 @@ public class LockScreenSettings extends SettingsPreferenceFragment
 
     // These switch preferences need special handling since they're not all stored in Settings.
     private static final String SWITCH_PREFERENCE_KEYS[] = { KEY_LOCK_AFTER_TIMEOUT,
-            KEY_LOCK_ENABLED, KEY_VISIBLE_PATTERN, KEY_VISIBLE_ERROR_PATTERN, KEY_VISIBLE_DOTS,
+            KEY_LOCK_ENABLED, KEY_VISIBLE_PATTERN, KEY_VISIBLE_ERROR_PATTERN, KEY_DIRECTLY_SHOW, KEY_VISIBLE_DOTS,
             KEY_VISIBLE_GESTURE, KEY_BIOMETRIC_WEAK_LIVELINESS, KEY_POWER_INSTANTLY_LOCKS };
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -139,6 +140,9 @@ public class LockScreenSettings extends SettingsPreferenceFragment
         }
         if (mVisibleErrorPattern != null) {
             mVisibleErrorPattern.setChecked(lockPatternUtils.isShowErrorPath());
+        }
+        if (mDirectlyShow != null) {
+            mDirectlyShow.setChecked(lockPatternUtils.shouldPassToSecurityView());
         }
         if (mVisibleDots != null) {
             mVisibleDots.setChecked(lockPatternUtils.isVisibleDotsEnabled());
@@ -203,6 +207,9 @@ public class LockScreenSettings extends SettingsPreferenceFragment
 
         // visible error pattern
         mVisibleErrorPattern = (SwitchPreference) root.findPreference(KEY_VISIBLE_ERROR_PATTERN);
+
+        // directly show
+        mDirectlyShow = (SwitchPreference) root.findPreference(KEY_DIRECTLY_SHOW);
 
         // visible dots
         mVisibleDots = (SwitchPreference) root.findPreference(KEY_VISIBLE_DOTS);
@@ -458,6 +465,8 @@ public class LockScreenSettings extends SettingsPreferenceFragment
             lockPatternUtils.setLockPatternEnabled((Boolean) value);
         } else if (KEY_VISIBLE_PATTERN.equals(key)) {
             lockPatternUtils.setVisiblePatternEnabled((Boolean) value);
+        } else if (KEY_DIRECTLY_SHOW.equals(key)) {
+            lockPatternUtils.setPassToSecurityView((Boolean) value);
         } else if (KEY_VISIBLE_ERROR_PATTERN.equals(key)) {
             lockPatternUtils.setShowErrorPath((Boolean) value);
         } else if (KEY_VISIBLE_DOTS.equals(key)) {
