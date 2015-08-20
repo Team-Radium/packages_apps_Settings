fe09f74712339c603b5e33c8e27674a6f6957fcb
randomlyjobless&bored <randomlybored@openmailbox.org>
2015-08-20 18:33:50 +0530

    selinux switch: use runShellCommand()
    Running it as the default user instead of root will remove the irratating toasts
    Also prevent Settings crashing if SuperSU not installed & root not enabled
    getenforce and getprop don't need root permissions
    
    Signed-off-by: randomlyjobless&bored <randomlybored@openmailbox.org>

--- src/com/android/settings/radium/RandomStuff.java
+++ src/com/android/settings/radium/RandomStuff.java
@@ -68,7 +68,7 @@ private SwitchPreference mSelinux;
         mSelinux = (SwitchPreference) findPreference(SELINUX);
         mSelinux.setOnPreferenceChangeListener(this);
 
-        if (CMDProcessor.runSuCommand("getenforce").getStdout().contains("Enforcing")) {
+        if (CMDProcessor.runShellCommand("getenforce").getStdout().contains("Enforcing")) {
             mSelinux.setChecked(true);
             mSelinux.setSummary(R.string.selinux_enforcing_title);
         } else {
